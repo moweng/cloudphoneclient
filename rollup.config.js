1a51bd1d76c408474954f9c9d544db04c2f7027a
moweng <1308490015@qq.com>
2023-07-31 22:29:47 +0800

    rollup打包逻辑更改

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,19 @@
-export default {
+
+const { defineConfig } = require('rollup');
+const typescript = require('rollup-plugin-typescript');
+const  path = require('path');
+module.exports = defineConfig({
     input: path.resolve(__dirname, './src/index.ts'),
     output: [
       {
         file: './dist/cloud-phone-client.js',
-        format: 'umd', // 通过umd格式输出
+        format: 'amd', // 通过amd格式输出
         name: 'CloudPhoneClient'
       }
+    ],
+    plugins: [
+      typescript({
+        sourceMap: true
+      })
     ]
-  }
+  });
